ed97ff3254aa5c8b1c695f910813e5a3ecf2d6e0
Volodymyr Popov <appforcelab@gmail.com>
2023-10-29 22:08:00 +0000

    add part 11

--- lib/app_blocs.dart
+++ lib/app_blocs.dart
@@ -5,7 +5,6 @@ import 'package:shop_fblock/app_states.dart';
 class AppBlocs extends Bloc<AppEvents, AppStates> {
   AppBlocs() : super(InitStates()) {
     on<Increment>((event, emit) {
-      print(' ++ ${state.counter}');
       emit(AppStates(counter: state.counter + 1));
     });
     on<Decrement>((event, emit) => emit(AppStates(counter: state.counter - 1)));
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shop_fblock/app_blocs.dart';
 import 'package:shop_fblock/app_events.dart';
 import 'package:shop_fblock/app_states.dart';
+import 'package:shop_fblock/pages/welcome/welcome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,37 +18,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AppBlocs(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
+      child: ScreenUtilInit(
+          designSize: const Size(360, 690),
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: const Welcome(),
+            );
+          }),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child: BlocBuilder<AppBlocs, AppStates>(builder: (context, state) {
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('ShopBlock'),
+      ),
+      body: Center(
+        child: BlocBuilder<AppBlocs, AppStates>(builder: (context, state) {
           return Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -59,23 +61,25 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           );
-        })),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            FloatingActionButton(
-              onPressed: () =>
-                  BlocProvider.of<AppBlocs>(context).add(Increment()),
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              onPressed: () =>
-                  BlocProvider.of<AppBlocs>(context).add(Decrement()),
-              tooltip: 'Decrement',
-              child: const Icon(Icons.remove),
-            )
-          ],
-        ));
+        }),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: () =>
+                BlocProvider.of<AppBlocs>(context).add(Increment()),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () =>
+                BlocProvider.of<AppBlocs>(context).add(Decrement()),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
   }
 }
